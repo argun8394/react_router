c554cb4a4e9b2904886b63bf7872ee77dddc702d
argun8394 <argundoganali@gmail.com>
2022-01-21 23:49:31 +0300

    useLocation_hook

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import {useParams,useLocation} from "react-router-dom";
 
 const Post= ({/*{match}*/})=> {
-    const {id} = useParams();
+    // const {id} = useParams();
+    const item = new URLSearchParams(useLocation().search);
 
     return(
         <div>
-            <h2>Post id : {id}</h2>
+            <h2>{item.get("name")}</h2>
+            <h2>{item.get("username")}</h2>
+            {/* <h2>Post id : {id}</h2> */}
              {/*<h2>Post id: {match.params.id}</h2>      useParams hooku ile "id" yalama işlemi yaptık yani post sonrası ne yazarsak ona erişiriz --- "name" yakalayacak olsaydık id yerine name yazacaktık */}
         </div>
     )
