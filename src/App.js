bbfb257361be53be8c60f591c2b337cc8804ff0e
argun8394 <argundoganali@gmail.com>
2022-01-21 22:54:37 +0300

    router_link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import Home from './pages/Home';
 
 const App=()=> {
   return (
-    <Router>
+    <Router forceRefresh>
+    {/* <Router forceRefresh basename="clarusway"> forceRefresh a tagi gibi sayfa yenilemesi sağlar
+    basename---url de yeralacak ana kısım için  */}
       <div className="App">
         <Header />
       </div>
@@ -16,6 +18,7 @@ const App=()=> {
         <Route exact path="/" component={Home} />{/*exact --> yalnızca "/" olduğunda bu sayfaya gitme işlemi sağlar  */}
         <Route path="/about" component={About} />
         <Route path="/profile" component={Profile} />
+        {/*<Route path="/" component={Home} />  exact a alternatif yöntem "/" ile ulaşılan sayfayı en sona koymak*/}
       </Switch>
     </Router>
   );
