bc98e9d6457ac3860cbce2b81c6704179b29d5a8
argun8394 <argundoganali@gmail.com>
2022-01-21 23:39:52 +0300

    useParams

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
 
-const Post= ({match})=> {
+const Post= ({/*{match}*/})=> {
+    const {id} = useParams();
+
     return(
         <div>
-             <h2>Post id: {match.params.id}</h2>{/*useParams ile "id" yalama işlemi yaptık "name" yakalayacak olsaydık id yerine name yazacaktık */}
+            <h2>Post id : {id}</h2>
+             {/*<h2>Post id: {match.params.id}</h2>      useParams hooku ile "id" yalama işlemi yaptık yani post sonrası ne yazarsak ona erişiriz --- "name" yakalayacak olsaydık id yerine name yazacaktık */}
         </div>
     )
 }
